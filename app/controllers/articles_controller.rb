f4375a2e68bde2a4243cd9a397830dc9615e3129
thangnpd <npdthang@gmail.com>
2017-07-07 10:29:40 +0700

    create find article id in private  method for some method call it because it's indentical

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,28 @@
 class ArticlesController < ApplicationController
+  before_action :find_id, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @articles = Article.all.order(:id).paginate(page: params[:page], per_page: 5)    
   end
  
   def show
-    @article = Article.find(params[:id])
+    
   end
  
   def new
+    console
     @article = Article.new
   end
  
   def edit
-    @article = Article.find(params[:id])
+    
   end
  
   def create
     @article = Article.new(article_params)
  
-    if @article.save      
-      @article.delay.status
+    if @article.save            
       redirect_to @article
     else
       render 'new'
@@ -27,8 +30,6 @@ class ArticlesController < ApplicationController
   end
  
   def update
-    @article = Article.find(params[:id])
- 
     if @article.update(article_params)
       redirect_to @article
     else
@@ -36,23 +37,25 @@ class ArticlesController < ApplicationController
     end
   end
  
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
- 
+  def destroy    
+    @article.destroy 
     redirect_to root
   end  
 
   def export_file
     @article = Article.find(params[:article_id])
-    @article.delay(run_at: 5.second.from_now).create_file(@article)
+    @article.delay(run_at: 30.seconds.from_now).create_file(@article)
     redirect_to articles_path
     
-    puts "redirect successssssssssssssssssssssssssssssssssss"    
+    puts "redirect successssssssssssssssssssssssssssssssssss"       
   end  
  
   private
     def article_params
       params.require(:article).permit(:title, :text, :avatar)
     end    
+
+    def find_id
+      @article = Article.find(params[:id])
+    end
 end
